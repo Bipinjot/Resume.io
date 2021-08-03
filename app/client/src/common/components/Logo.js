13a89cf800bd517e1b8744e4a0543bdaa9875010
Bipinjot Kaur Hara <Bipinjot@users.noreply.github.com>
2021-08-04 00:53:56 +0530

    First commit

---
+++ app/client/src/common/components/Logo.js
@@ -0,0 +1,40 @@
+/**
+ * @flow
+ */
+
+import React from 'react'
+import { Link } from 'react-router-dom'
+import styled from 'styled-components'
+import { colors } from '../theme'
+
+const Text = styled.h1`
+  font-family: NexaBold;
+  font-size: ${props => (props.big ? 4 : 2.5)}em;
+  margin: 0;
+`
+
+const StyledLink = styled(Link)`
+  text-decoration: none;
+  color: white;
+`
+
+const Accent = styled.em`
+  font-style: normal;
+  color: ${colors.primary};
+`
+
+type Props = {
+  big?: boolean
+}
+
+function Logo({ big }: Props) {
+  return (
+    <Text big={big}>
+      <StyledLink to="/">
+        Higheredd's<Accent> ResumeBuilder</Accent>
+      </StyledLink>
+    </Text>
+  )
+}
+
+export default Logo
